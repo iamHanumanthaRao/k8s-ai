5eb766f99f9dfac9dbca95b0f21b0286085ca31d
Hanumantha Rao Bandarupalli <iamhanumanth@outlook.com>
2023-09-22 17:41:51 +0530

    Final Commit

--- main.py
+++ main.py
@@ -21,30 +21,41 @@ st.set_page_config(
    initial_sidebar_state="expanded",
 )
 
+def showRadio():
+    cluster = st.radio(
+    "Tell AI the cluster you wanted",
+    [":rainbow[Dev]", ":rainbow[QA]", ":rainbow[PP]", ":rainbow[DR]"], horizontal = True
+    # captions = ["Laugh out loud.", "Get the popcorn.", "Never stop learning."]
+    )
+    if cluster == ':rainbow[Dev]':
+        os.system('kubectl config use-context arn:aws:eks:us-east-1:751115992403:cluster/dev-01')
+        st.write('You selected Dev Cluster.')
+    elif cluster == ':rainbow[QA]':
+        os.system('kubectl config use-context arn:aws:eks:us-east-1:751115992403:cluster/qa-01')
+        st.write("You selected QA Cluster.")
+    elif cluster == ':rainbow[DR]':
+        os.system('kubectl config use-context arn:aws:eks:us-west-2:445819549122:cluster/DR-EKS')
+        st.write("You selected DR Cluster.")
+    else:
+        os.system('kubectl config use-context arn:aws:eks:us-east-1:751115992403:cluster/pp-01')
+        st.write("You selected PP Cluster.")
+
 image = Image.open('Signeasy.png')
 st.image("Signeasy.png", use_column_width=False)
 
-genre = st.radio(
-    "Tell AI the cluster you wanted",
-    [":rainbow[Dev]", ":rainbow[QA]", ":rainbow[PP]", ":rainbow[DR]"]
-    # captions = ["Laugh out loud.", "Get the popcorn.", "Never stop learning."]
+infra = st.radio(
+        "Select the infra",
+        ("Kubernetes", "AWS")
     )
 
-if genre == ':rainbow[Dev]':
-    os.system('kubectl config use-context arn:aws:eks:us-east-1:751115992403:cluster/dev-01')
-    st.write('You selected Dev Cluster.')
-elif genre == ':rainbow[QA]':
-    os.system('kubectl config use-context arn:aws:eks:us-east-1:751115992403:cluster/qa-01')
-    st.write("You selected QA Cluster.")
-elif genre == ':rainbow[DR]':
-    os.system('kubectl config use-context arn:aws:eks:us-west-2:445819549122:cluster/DR-EKS')
-    st.write("You selected DR Cluster.")
-else:
-    os.system('kubectl config use-context arn:aws:eks:us-east-1:751115992403:cluster/pp-01')
-    st.write("You selected PP Cluster.")
-
-st.title('Alfred - AI ')
-input_text=st.text_input("What would you like to know today ?")
+if infra == 'Kubernetes':
+    st.write('Selected Infra is Kuberenetes')
+    showRadio()
+    st.title('Alfred - AI ')
+    input_text=st.text_input("What would you like to know today ?")
+elif infra == 'AWS':
+    st.title('Infra - AI ')
+    input_text=st.text_input("What would you like to know today ?")
 
 def writePods(info):
     env = Environment(loader=FileSystemLoader(searchpath="."))
@@ -53,7 +64,7 @@ def writePods(info):
     headers = re.split(r'\s+', lines[0]) if lines else []
     rows = [re.split(r'\s+', line, maxsplit=len(headers)-1) for line in lines[1:]]
     rendered_html = template.render(headers=headers, rows=rows)
-    components.v1.html(rendered_html, width=1200, height=600)    
+    components.v1.html(rendered_html, height=600)    
 
 def getInfo(cmd):
     try:
@@ -62,13 +73,13 @@ def getInfo(cmd):
         writePods(pods_info)
     except subprocess.CalledProcessError as e:
         print(f"Error occurred: {e.stderr}")
-        st.write("Please conect to the right VPN or check your Internet Connection")
+        st.write("Please conect to the right VPN or check your Internet Connection or I am not there yet to understand the question :-)")
 
 def gptInfo(input_text):
     keyWords = ["erase", "delete", "remove", "Delete", "DELETE"]
     first_input_prompt=PromptTemplate(
         input_variables=['command'],
-        template="Command in k8s for {command}"
+        template="Command in k8s for {command} respond with command as plain text"
     )
     ## OPENAI LLMS
     llm=OpenAI(temperature=0.8)
@@ -76,11 +87,11 @@ def gptInfo(input_text):
     if input_text:
         for key in keyWords:
             if key in input_text:
-                st.write("I don't have capabilites to perform this action")
+                st.write("I am not permitted to perform this action")
                 sys.exit()                   
         out = chain.run(input_text)
         print(out)
-        st.write(out)
+        # st.write(out)
         getInfo(out)
 
 gptInfo(input_text)
